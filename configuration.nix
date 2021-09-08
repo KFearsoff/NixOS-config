658916bfa4891ae20d997d7313e3e468bcb08d9d
KFears <kfearsoff@gmail.com>
2021-09-08 17:00:36 +0300

    add basic configuration and flakes

---
+++ configuration.nix
@@ -0,0 +1,149 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    trustedUsers = [ "root" "user" ];
+    autoOptimiseStore = true;
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_CTYPE="en_US.UTF-8";
+    LC_NUMERIC="en_US.UTF-8";
+    LC_TIME="en_GB.UTF-8";
+    LC_COLLATE="en_US.UTF-8";
+    LC_MONETARY="en_US.UTF-8";
+    LC_MESSAGES="en_US.UTF-8";
+    LC_PAPER="en_US.UTF-8";
+    LC_NAME="en_US.UTF-8";
+    LC_ADDRESS="en_US.UTF-8";
+    LC_TELEPHONE="en_US.UTF-8";
+    LC_MEASUREMENT="en_US.UTF-8";
+    LC_IDENTIFICATION="en_US.UTF-8";
+  };
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.displayManager.defaultSession = "none+plasma5";
+
+  
+
+  # Configure keymap in X11
+  services.xserver.layout = "us,ru";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    qutebrowser
+    freetube
+    tdesktop
+    htop
+    xorg.xkill
+    neofetch
+    vlc
+    git
+    ungoogled-chromium
+    notepadqq
+    anydesk
+    samba
+    cifs-utils
+    lxqt.lxqt-policykit
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "NixOS configuration with flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ { self, nixpkgs, nixos-hardware, home-manager, nixpkgs-unstable, ... }:
+    let
+      inherit (nixpkgs) lib;
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+        nixpkgs.config.allowUnfree = true;
+        unstable = (import nixpkgs-unstable {
+          inherit system;
+          config = { allowUnfree = true; };
+        });
+      };
+      buildConfig = modules: { inherit modules system specialArgs; };
+      buildSystem = modules: lib.nixosSystem (buildConfig modules);
+    in
+      {
+      nixosConfigurations = {
+        nixos = buildSystem [
+          ./configuration.nix
+          ./hardware-configuration.nix
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc-ssd
+          nixos-hardware.nixosModules.common-pc
+        ];  
+      };
+    };
+}
