ad74074ef94dd7fe92f34196dce0497421d86060
KFearsoff <kfearsoff@gmail.com>
2022-05-02 20:50:44 +0500

    enableRedistributableFirmware

--- hosts/blackberry/hardware-configuration.nix
+++ hosts/blackberry/hardware-configuration.nix
@@ -55,4 +55,5 @@
   swapDevices = [ ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = true;
 }
--- hosts/blueberry/hardware-configuration.nix
+++ hosts/blueberry/hardware-configuration.nix
@@ -58,4 +58,5 @@
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = true;
 }
