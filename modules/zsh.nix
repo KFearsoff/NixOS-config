7a0fc0dfdb640f68e8df028e20ab8710a0d050f9
KFearsoff <kfearsoff@gmail.com>
2022-05-18 14:34:17 +0300

    add more functions to zsh

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -52,18 +52,77 @@
           fe() {
             rg --files ''${1:-.} | fzf --preview 'bat -fp {}' | xargs $EDITOR
           }
+
+          # Search content and Edit
+          se() {
+            fileline=$(rg -n ''${1:-.} | fzf | awk '{print $1}' | sed 's/.$//')
+            $EDITOR ''${fileline%%:*} +''${fileline##*:}
+          }
+
+          # Search git log, preview shows subject, body, and diff
+          fl() {
+            git log --oneline --color=always | fzf --ansi --preview="echo {} | cut -d ' ' -f 1 | xargs -I @ sh -c 'git log --pretty=medium -n 1 @; git diff @^ @' | bat --color=always" | cut -d ' ' -f 1 | xargs git log --pretty=short -n 1
+          }
+
           # Autocomplete "flake" to "flake.nix"
             zstyle ":completion:*:*:vim:*:*files" ignored-patterns '*.lock'
+
           # Highlight --help with bat 
           help() {
               "$@" --help 2>&1 | bat -pl help
             }
+
           batcopy() {
             bat "$@" | wl-copy
             }
+
           battail() {
               tail -f "$@" | bat --paging=never -pl log
             }
+
+
+
+          SAVEIFS=$IFS
+          IFS=$(echo -en "\n\b")
+
+          function extract {
+           if [ -z "$1" ]; then
+              # display usage if no parameters given
+              echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+              echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+           else
+              for n in "$@"
+              do
+                if [ -f "$n" ] ; then
+                    case "''${n%,}" in
+                      *.cbt|*.tar.bz2|*.tar.gz|*.tar.xz|*.tbz2|*.tgz|*.txz|*.tar)
+                                   tar xvf "$n"       ;;
+                      *.lzma)      unlzma ./"$n"      ;;
+                      *.bz2)       bunzip2 ./"$n"     ;;
+                      *.cbr|*.rar)       unrar x -ad ./"$n" ;;
+                      *.gz)        gunzip ./"$n"      ;;
+                      *.cbz|*.epub|*.zip)       unzip ./"$n"       ;;
+                      *.z)         uncompress ./"$n"  ;;
+                      *.7z|*.arj|*.cab|*.cb7|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar)
+                                   7z x ./"$n"        ;;
+                      *.xz)        unxz ./"$n"        ;;
+                      *.exe)       cabextract ./"$n"  ;;
+                      *.cpio)      cpio -id < ./"$n"  ;;
+                      *.cba|*.ace)      unace x ./"$n"      ;;
+                      *)
+                                   echo "extract: '$n' - unknown archive method"
+                                   return 1
+                                   ;;
+                    esac
+                else
+                    echo "'$n' - file does not exist"
+                    return 1
+                fi
+              done
+          fi
+          }
+
+          IFS=$SAVEIFS
         '';
         plugins = [
           {
