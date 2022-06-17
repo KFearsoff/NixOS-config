eb1e720380091a5607c7a700800a83de4ca7c2ee
KFearsoff <kfearsoff@gmail.com>
2022-06-18 01:52:55 +0500

    refactor: flake, Makefile->Devshell, pre-commit->0

--- flake.nix
+++ flake.nix
@@ -2,90 +2,74 @@
   description = "NixOS configuration with flakes";
 
   inputs = {
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.utils.follows = "flake-utils";
     home-manager.inputs.flake-compat.follows = "flake-compat";
+
     home-manager-newsboat.url = "github:nix-community/home-manager/fb82568aa899f947a26d4d0c01cb30960f53f182";
-    home-manager-newsboat.inputs.nixpkgs.follows = "unstable";
+    home-manager-newsboat.inputs.nixpkgs.follows = "nixpkgs";
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    neovim-nightly-overlay.inputs.nixpkgs.follows = "unstable";
-    neovim-flake.url = "github:neovim/neovim?dir=contrib";
-    neovim-flake.inputs.nixpkgs.follows = "unstable";
-    neovim-flake.inputs.flake-utils.follows = "flake-utils";
+    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
     neovim-nightly-overlay.inputs.flake-compat.follows = "flake-compat";
     neovim-nightly-overlay.inputs.neovim-flake.follows = "neovim-flake";
 
-    neovim-lsp.url = "github:neovim/nvim-lspconfig";
-    neovim-lsp.inputs.nixpkgs.follows = "unstable";
-    neovim-lsp.inputs.flake-utils.follows = "flake-utils";
+    neovim-flake.url = "github:neovim/neovim?dir=contrib";
+    neovim-flake.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-flake.inputs.flake-utils.follows = "flake-utils";
 
-    zsh-you-should-use.url = "github:MichaelAquilina/zsh-you-should-use";
-    zsh-you-should-use.flake = false;
     nur.url = "github:nix-community/NUR";
     nix-colors.url = "github:misterio77/nix-colors";
     impermanence.url = "github:nix-community/impermanence";
 
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.nixpkgs.follows = "unstable";
-    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.inputs.flake-utils.follows = "flake-utils";
 
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "unstable";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+
+    zsh-you-should-use.url = "github:MichaelAquilina/zsh-you-should-use";
+    zsh-you-should-use.flake = false;
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    unstable,
     flake-utils,
     hardware,
     home-manager,
     neovim-nightly-overlay,
     nur,
     nix-colors,
-    pre-commit-hooks,
     impermanence,
+    devshell,
     sops-nix,
     ...
   }: let
-    buildSystem = system: pkgs: extraModules:
+    buildSystem = {
+      system ? "x86_64-linux",
+      pkgs ? nixpkgs,
+      username ? "nixchad",
+      extraModules,
+    }:
       pkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = {
-          inherit inputs nix-colors;
-          username = "nixchad";
-        };
+        specialArgs = {inherit inputs nix-colors username;};
         modules =
           [
             home-manager.nixosModules.home-manager
             impermanence.nixosModules.impermanence
             sops-nix.nixosModules.sops
-            {
-              nix = {
-                extraOptions = ''
-                  flake-registry = /etc/nix/registry.json
-                '';
-                registry = {
-                  self.flake = inputs.self;
-                  nixpkgs = {
-                    from = {
-                      id = "nixpkgs";
-                      type = "indirect";
-                    };
-                    flake = pkgs;
-                  };
-                };
-              };
-            }
+            (./users + "/${username}.nix")
           ]
           ++ extraModules;
       };
@@ -97,45 +81,36 @@
         neovim-nightly-overlay = neovim-nightly-overlay.overlay;
       };
       nixosConfigurations = {
-        blueberry =
-          buildSystem "x86_64-linux" unstable
-          [
+        blueberry = buildSystem {
+          extraModules = [
             {nixpkgs.overlays = builtins.attrValues self.overlays;}
             ./hosts/blueberry
-            ./users/nixchad.nix
             ./suites
           ];
+        };
 
-        blackberry =
-          buildSystem "x86_64-linux" unstable
-          [
+        blackberry = buildSystem {
+          extraModules = [
             {nixpkgs.overlays = builtins.attrValues self.overlays;}
             ./hosts/blackberry
-            ./users/nixchad.nix
             ./suites
           ];
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (system: {
-      formatter = unstable.legacyPackages.${system}.alejandra;
-
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            statix.enable = true;
-            alejandra.enable = true;
-          };
         };
       };
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
 
-      devShell = unstable.legacyPackages.${system}.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-
-        buildInputs = with unstable.legacyPackages.${system}.pkgs; [
-          sops
-          gnumake
-        ];
-      };
+      devShell = let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [devshell.overlay];
+        };
+      in
+        pkgs.devshell.mkShell {
+          imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+        };
     });
 }
