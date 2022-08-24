48cead24d761046adfb2bafea3f1c69d87f3c3c7
KFearsoff <kfearsoff@gmail.com>
2022-08-24 13:08:30 +0400

    feat: add ipv6 to coredns

--- modules/service-private/coredns.nix
+++ modules/service-private/coredns.nix
@@ -18,25 +18,25 @@ in {
       config = ''
         .:5053 {
           prometheus localhost:33003
-          forward . 127.0.0.1:5054 127.0.0.1:5055 127.0.0.1:5056
+          forward . 127.0.0.1:5054 [::1]:5054 127.0.0.1:5055 [::1]:5055 127.0.0.1:5056 [::1]:5056
           errors
           cache
         }
 
         .:5054 {
-          forward . 1.1.1.1 1.0.0.1 {
+          forward . 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001 {
             tls_servername cloudflare-dns.com
           }
         }
 
         .:5055 {
-          forward . 9.9.9.9 149.112.112.112 {
+          forward . 9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9 {
             tls_servername dns.quad9.net
           }
         }
 
         .:5056 {
-          forward . 8.8.8.8 8.8.4.4 {
+          forward . 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 {
             tls_servername dns.google
           }
         }
@@ -53,6 +53,6 @@ in {
       allowedUDPPorts = [5053];
     };
 
-    networking.nameservers = mkForce ["127.0.0.1:5053"];
+    networking.nameservers = mkForce ["127.0.0.1:5053" "[::1]:5053"];
   };
 }
