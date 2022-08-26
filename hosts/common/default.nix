2e60323e70d22bfa8eae96dd21e3e1c6ba5b5344
KFearsoff <kfearsoff@gmail.com>
2022-08-26 18:06:53 +0400

    fix: don't overwrite local DNS with Tailscale

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -20,7 +20,7 @@
     fallbackDns = ["9.9.9.9" "8.8.8.8" "1.1.1.1"];
     dnssec = "false"; # we already DNSSEC on CoreDNS
   };
-  networking.nameservers = lib.mkForce [];
+  networking.nameservers = ["127.0.0.1"];
 
   # https://libredd.it/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a/
   #systemd.targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
