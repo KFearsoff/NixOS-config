ec0ac1b83ffd0b498bf5c7d356fad6900f0e6cc4
KFearsoff <kfearsoff@gmail.com>
2022-08-28 12:32:18 +0400

    feat: use hostnames not IPs for syncthing

--- hosts/common/syncthing.nix
+++ hosts/common/syncthing.nix
@@ -13,19 +13,19 @@
     devices = {
       "blueberry" = {
         id = "O3BLXFF-YZDZXSU-MZMNFVX-WDQ2OZU-3UZ7SSK-4BEN6DU-COS4LM4-7MP6QAQ";
-        addresses = ["tcp://100.66.71.32" "quic://100.66.71.32"];
+        addresses = ["tcp://blueberry" "quic://blueberry"];
       };
       "blackberry" = {
         id = "S3S7WIB-2J2YK4E-VIKFG4K-FZ7N4OI-43RXU3D-T5FX3AD-PQCJPSX-LNQNRA5";
-        addresses = ["tcp://100.109.194.31" "quic://100.109.194.31"];
+        addresses = ["tcp://blackberry" "quic://blackberry"];
       };
       "pixel-4a" = {
         id = "74LQXWB-GVD5FVU-7CYZUFK-MNIIYA4-X3ZJCHR-VQM7UM7-HBNL4VM-PUNYTAW";
-        addresses = ["tcp://100.107.161.19" "quic://100.107.161.19"];
+        addresses = ["tcp://pixel-4a" "quic://pixel-4a"];
       };
       "virtberry" = {
         id = "LIT3MIA-VEPVHWZ-SC4Z76E-V5YFUV7-IF4TKR2-VOVI3E5-BKXIHFB-TYQE3QH";
-        addresses = ["tcp://100.94.96.106" "quic://100.94.96.106"];
+        addresses = ["tcp://virtberry" "quic://virtberry"];
       };
     };
     folders = {
