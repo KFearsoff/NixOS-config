cc2c94d801266ea0b707ae20d3c0797edb3f5074
KFearsoff <kfearsoff@gmail.com>
2022-10-31 13:09:49 +0400

    refactor: ssh use metadata.toml

--- hosts/default.nix
+++ hosts/default.nix
@@ -5,9 +5,11 @@ let
   metadataTypes = builtins.attrNames metadata;
   metadataNoTypes = builtins.foldl' (x: y: x // metadata."${y}") {} metadataTypes;
   syncthingEntries = filterAttrs (n: v: builtins.hasAttr "syncthing" v) metadataNoTypes;
+  sshPubkeyEntries = filterAttrs (n: v: builtins.hasAttr "ssh-pubkey" v) metadataNoTypes;
 in {
   inherit metadata;
   syncthingDevicesConfig = mapAttrs (n: v: v.syncthing // { addresses = ["tcp://${n}" "quic://${n}"];}) syncthingEntries;
   syncthingHostsList = builtins.attrNames (filterAttrs (n: v: builtins.hasAttr "syncthing" v) metadata.hosts);
   syncthingAllList = builtins.attrNames syncthingEntries;
+  sshPubkeyList = (zipAttrs (attrValues sshPubkeyEntries)).ssh-pubkey;
 }
