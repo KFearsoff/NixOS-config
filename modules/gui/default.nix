6355b73d26104c4c71d15f5302ea1bcb99d20edd
KFearsoff <kfearsoff@gmail.com>
2022-11-03 00:14:49 +0400

    refactor: move gui to modules

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,6 +1,8 @@
 {
   config,
   lib,
+  pkgs,
+  username,
   ...
 }:
 with lib; let
@@ -14,5 +16,42 @@ in {
 
   options.nixchad.gui = {
     enable = mkEnableOption "gui programs";
+    obs = mkEnableOption "obs";
+    rofi = mkEnableOption "rofi";
+  };
+
+  config = mkIf cfg.enable {
+    nixchad.gui.obs = mkDefault true;
+    nixchad.gui.rofi = mkDefault true;
+
+    environment.systemPackages = with pkgs; [
+      xdg-utils
+    ];
+
+    home-manager.users."${username}" = {
+      home.packages = with pkgs; [
+        ungoogled-chromium
+        freetube
+        tdesktop
+        keepassxc
+        qbittorrent
+        element-desktop-wayland
+        cinnamon.nemo
+        obsidian
+      ]
+      ++ (optional config.services.pipewire.enable easyeffects)
+      ++ (optional config.programs.wireshark.enable wireshark);
+
+      services.udiskie.enable = true;
+      programs = {
+        obs-studio.enable = cfg.obs;
+        rofi = {
+          enable = cfg.rofi;
+          package = pkgs.rofi-wayland;
+          theme = "purple";
+          terminal = "alacritty";
+        };
+      };
+    };
   };
 }
--- profiles/gui/default.nix
+++ profiles/gui/default.nix
@@ -1,56 +1,7 @@
 {
-  username,
-  config,
-  pkgs,
-  ...
-}: {
   imports = [
     ./alacritty.nix
     ./mpv.nix
     ./zathura.nix
   ];
-
-  config.home-manager.users."${username}" = {config, ...}: {
-    home = {
-      packages = with pkgs; [
-        ungoogled-chromium
-        freetube
-        tdesktop
-        keepassxc
-        discord
-        qbittorrent
-        element-desktop-wayland
-        cinnamon.nemo
-        obsidian
-        easyeffects
-        xdg-utils
-        wireshark
-        anydesk
-      ];
-    };
-
-    programs = {
-      obs-studio.enable = true;
-      rofi = {
-        enable = true;
-        package = pkgs.rofi-wayland;
-        theme = "purple";
-        terminal = "alacritty";
-      };
-    };
-
-    services.udiskie.enable = true;
-    services.mpd = {
-      enable = true;
-      network.startWhenNeeded = true;
-      dataDir = "${config.home.homeDirectory}/.config/mpd";
-      musicDirectory = "${config.home.homeDirectory}/Music";
-      extraConfig = ''
-        audio_output {
-            type "pipewire"
-            name "Pipewire"
-          }
-      '';
-    };
-  };
 }
