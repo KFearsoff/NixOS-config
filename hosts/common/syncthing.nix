c6f0811c11911616138121f749b7667b9775eabd
KFearsoff <66781795+KFearsoff@users.noreply.github.com>
2022-11-02 14:21:31 +0300

    feat: create metadata file (#17)
    * feat: add metadata.toml
    
    * refactor(syncthing config): use metadata.toml
    
    * refactor(syncthing folders): use metadata.toml
    
    * refactor(syncthing folders): use all metadata.toml
    
    * refactor: metadata.toml usage
    
    * refactor: ssh use metadata.toml

--- hosts/common/syncthing.nix
+++ hosts/common/syncthing.nix
@@ -14,35 +14,18 @@ in {
     XDG_PROJ_DIR = "$HOME/Projects";
   };
 
-  services.syncthing = {
+  services.syncthing = with (import ../default.nix { inherit lib;}); {
     enable = true;
     overrideDevices = true;
     overrideFolders = true;
     user = "${username}";
     dataDir = "/home/${username}";
-    devices = {
-      "blueberry" = {
-        id = "O3BLXFF-YZDZXSU-MZMNFVX-WDQ2OZU-3UZ7SSK-4BEN6DU-COS4LM4-7MP6QAQ";
-        addresses = ["tcp://blueberry" "quic://blueberry"];
-      };
-      "blackberry" = {
-        id = "S3S7WIB-2J2YK4E-VIKFG4K-FZ7N4OI-43RXU3D-T5FX3AD-PQCJPSX-LNQNRA5";
-        addresses = ["tcp://blackberry" "quic://blackberry"];
-      };
-      "pixel-4a" = {
-        id = "74LQXWB-GVD5FVU-7CYZUFK-MNIIYA4-X3ZJCHR-VQM7UM7-HBNL4VM-PUNYTAW";
-        addresses = ["tcp://pixel-4a" "quic://pixel-4a"];
-      };
-      "virtberry" = {
-        id = "LIT3MIA-VEPVHWZ-SC4Z76E-V5YFUV7-IF4TKR2-VOVI3E5-BKXIHFB-TYQE3QH";
-        addresses = ["tcp://virtberry" "quic://virtberry"];
-      };
-    };
+    devices = syncthingDevicesConfig;
     folders =
       {
         ".config/newsboat" = {
           path = "/home/${username}/.config/newsboat";
-          devices = ["blueberry" "blackberry" "virtberry"];
+          devices = syncthingHostsList;
           versioning = {
             type = "trashcan";
             params.cleanoutDays = "30";
@@ -50,7 +33,7 @@ in {
         };
         "Sync" = {
           path = "/home/${username}/Sync";
-          devices = ["blueberry" "blackberry" "virtberry"];
+          devices = syncthingHostsList;
           versioning = {
             type = "trashcan";
             params.cleanoutDays = "30";
@@ -58,7 +41,7 @@ in {
         };
         "Projects" = {
           path = "/home/${username}/Projects";
-          devices = ["blueberry" "blackberry" "virtberry"];
+          devices = syncthingHostsList;
           versioning = {
             type = "trashcan";
             params.cleanoutDays = "30";
@@ -66,7 +49,7 @@ in {
         };
         "Notes" = {
           path = "/home/${username}/Documents/Notes";
-          devices = ["blueberry" "blackberry" "pixel-4a" "virtberry"];
+          devices = syncthingAllList;
           versioning = {
             type = "trashcan";
             params.cleanoutDays = "30";
@@ -78,11 +61,11 @@ in {
         then {
           "Photos" = {
             path = "/home/${username}/Pictures/Photos";
-            devices = ["blueberry" "blackberry"];
+            devices = lib.remove "virtberry" syncthingHostsList;
           };
           "Photos-phone" = {
             path = "/home/${username}/Pictures/Photos-phone";
-            devices = ["blueberry" "blackberry" "pixel-4a"];
+            devices = lib.remove "virtberry" syncthingAllList;
             type = "receiveonly";
             versioning = {
               type = "trashcan";
