4c93e039a5617a11271791e5b1b9aa4da1ba36ce
KFearsoff <kfearsoff@gmail.com>
2022-11-07 11:15:54 +0400

    feat: vaultwarden -> tailscale https and postgres

--- modules/private-services/vaultwarden.nix
+++ modules/private-services/vaultwarden.nix
@@ -6,38 +6,61 @@
 with lib; let
   cfg = config.nixchad.vaultwarden;
   hostname = config.networking.hostName;
-  vaultwardenPort = "32003";
-  vaultwardenDomain = "vaultwarden.${hostname}.box";
+  vaultwardenPort = 32003;
+  domain = "${hostname}.tail34ad.ts.net";
 in {
   options.nixchad.vaultwarden = {
     enable = mkEnableOption "Vaultwarden";
   };
 
   config = mkIf cfg.enable {
-    services.vaultwarden.enable = true;
-    services.vaultwarden.config = {
-      DOMAIN = "https://${vaultwardenDomain}";
-      ROCKET_PORT = vaultwardenPort;
+    services.vaultwarden = {
+      enable = true;
+      dbBackend = "postgresql";
+      config = {
+        rocketAddress = "0.0.0.0";
+        rocketPort = vaultwardenPort;
+
+        websocketEnabled = true;
+        websocketAddress = "0.0.0.0";
+        websocketPort = 3012;
+
+        databaseUrl = "postgresql://vaultwarden@/vaultwarden";
+
+        domain = "https://${domain}/vault";
+      };
     };
 
-    # don't use SSL certs
-    services.nginx.virtualHosts."${vaultwardenDomain}" = {
-      forceSSL = true;
-      sslCertificate = "/var/lib/self-signed/_.blackberry.box/cert.pem";
-      sslCertificateKey = "/var/lib/self-signed/_.blackberry.box/key.pem";
+    services.postgresql = {
+      ensureDatabases = ["vaultwarden"];
+      ensureUsers = [
+        {
+          name = "vaultwarden";
+          ensurePermissions."DATABASE vaultwarden" = "ALL PRIVILEGES";
+        }
+      ];
+    };
 
-      locations."/" = {
-        proxyPass = "http://localhost:${vaultwardenPort}";
+    services.nginx.virtualHosts."${domain}" = {
+      forceSSL = true;
+      sslCertificate = "/var/lib/self-signed/${domain}.crt";
+      sslCertificateKey = "/var/lib/self-signed/${domain}.key";
+
+      locations."/vault" = {
+        proxyPass = "http://localhost:${toString vaultwardenPort}";
         proxyWebsockets = true;
       };
-      locations."/notifications/hub" = {
+      locations."/vault/notifications/hub" = {
         proxyPass = "http://localhost:3012";
         proxyWebsockets = true;
       };
-      locations."/notifications/hub/negotiate" = {
-        proxyPass = "http://localhost:${vaultwardenPort}";
+      locations."/vault/notifications/hub/negotiate" = {
+        proxyPass = "http://localhost:${toString vaultwardenPort}";
         proxyWebsockets = true;
       };
     };
+
+    # make sure we don't crash because postgres isn't ready
+    systemd.services.vaultwarden.after = ["postgresql.service"];
   };
 }
