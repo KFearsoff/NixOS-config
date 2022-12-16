e3de4fa7caf5c5fc8bfc40441349e908b3c6cede
KFearsoff <kfearsoff@gmail.com>
2022-12-16 20:18:55 +0400

    feat: remove hosts from coredns

--- modules/common-services/coredns/default.nix
+++ modules/common-services/coredns/default.nix
@@ -2,7 +2,6 @@
 # https://github.com/ckiee/ckiesite-static/blob/3bbbc4d31691e9554c817d9bd8792e4e13a8e292/public/tailscale-coredns.md
 # https://github.com/ckiee/nixfiles/tree/6d130dd21aab21792d775692218be2e073a6eb81/modules/services/coredns
 {
-  inputs,
   config,
   lib,
   pkgs,
@@ -10,64 +9,12 @@
 }:
 with lib; let
   cfg = config.nixchad.coredns;
-  hostSuffix = ".tailnet.nixchad.dev";
-
-  # Taken from upstream: https://github.com/StevenBlack/hosts/blob/master/flake.nix
-  alternatesList =
-    lib.optional cfg.blockFakenews "fakenews"
-    ++ lib.optional cfg.blockGambling "gambling"
-    ++ lib.optional cfg.blockPorn "port"
-    ++ lib.optional cfg.blockSocial "social";
-  alternatesPath = "alternates/" + builtins.concatStringsSep "-" alternatesList + "/";
-
-  combinedHosts = builtins.readFile ("${inputs.hosts}/"
-    + lib.optionalString (alternatesList != []) alternatesPath
-    + "hosts");
-  excludeHosts = concatStringsSep "|" [".*kameleoon\..+"];
-  patchedHosts = concatStringsSep "\n" (filter (x: ((builtins.match excludeHosts x) == null)) (splitString "\n" combinedHosts));
-  magicDNS = config.lib.metadata.magicDNS hostSuffix;
-
-  baseHosts = pkgs.writeTextFile {
-    name = "coredns-hosts-nixchad";
-    text = ''
-      # Custom hosts
-      ${patchedHosts}
-      # Extra hosts
-      ${cfg.extraHosts}
-      # Imitate MagicDNS
-      ${magicDNS}
-    '';
-  };
 in {
   options.nixchad.coredns = {
     enable = mkEnableOption "CoreDNS server";
-
-    extraHosts = mkOption {
-      type = types.lines;
-      default = "";
-      description = "Extra hosts separated by lines";
-    };
-
-    blockFakenews = mkEnableOption "Additionally block fakenews hosts.";
-    blockGambling = mkEnableOption "Additionally block gambling hosts.";
-    blockPorn = mkEnableOption "Additionally block porn hosts.";
-    blockSocial = mkEnableOption "Additionally block social hosts.";
   };
 
   config = mkIf cfg.enable {
-    systemd.services.dns-hosts-update = {
-      description = "Update the /run/coredns-hosts hosts file with new Tailscale hosts";
-      wantedBy = ["multi-user.target"];
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = pkgs.writeShellScript "dns-hosts-update" ''
-          rm /run/coredns-hosts || true
-          ln -s ${baseHosts} /run/coredns-hosts
-        '';
-      };
-    };
-
     services.coredns = {
       enable = true;
       package = pkgs.coredns.override {
@@ -83,11 +30,6 @@ in {
           # We can't bind to 0.0.0.0 (default) because we'll have conflicts with
           # systemd-resolved and dnsmasq (for libvirtd)
           bind lo tailscale0
-
-          hosts /run/coredns-hosts {
-            reload 1500ms
-            fallthrough
-          }
 
           fanout . 127.0.0.1:5054 [::1]:5054 127.0.0.1:5055 [::1]:5055 127.0.0.1:5056 [::1]:5056
           errors
