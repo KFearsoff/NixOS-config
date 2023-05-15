eec4caac8e590884a962d3480f361616a0b3f898
KFearsoff <kfearsoff@gmail.com>
2023-05-15 11:10:22 +0400

    feat: add grafana auth

--- modules/private-services/default.nix
+++ modules/private-services/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   servername,
   ...
@@ -58,6 +59,9 @@ in {
       recommendedBrotliSettings = true;
       recommendedGzipSettings = true;
       recommendedProxySettings = true;
+      recommendedZstdSettings = true;
+
+      statusPage = true;
     };
 
     services.prometheus.exporters.nginx = {
@@ -67,5 +71,21 @@ in {
 
     networking.firewall.allowedTCPPorts = [80 443];
     networking.firewall.allowedUDPPorts = [80 443];
+
+    systemd.services."tailscale.nginx-auth" = {
+      after = ["nginx.service"];
+      wants = ["nginx.service"];
+      wantedBy = ["default.target"];
+      serviceConfig = {
+        ExecStart = "${pkgs.tailscale}/bin/nginx-auth";
+        DynamicUser = true;
+      };
+    };
+
+    systemd.sockets."tailscale.nginx-auth" = {
+      partOf = ["tailscale.nginx-auth.service"];
+      wantedBy = ["sockets.target"];
+      listenStreams = ["/run/tailscale/tailscale.nginx-auth.sock"];
+    };
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,4 @@
 {
   additions = final: _: import ../pkgs {pkgs = final;};
+  tailscale-overlay = _: prev: {tailscale = prev.tailscale.overrideAttrs (_: {subPackages = ["cmd/tailscale" "cmd/tailscaled" "cmd/nginx-auth"];});};
 }
