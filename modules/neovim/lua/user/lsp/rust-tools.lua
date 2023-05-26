460e4b2fcd93bfba9cd9b1c4cabfb894de5c69ad
KFearsoff <kfearsoff@gmail.com>
2023-05-26 22:26:19 +0400

    perf: improve rust-tools perf

--- modules/neovim/lua/user/lsp/rust-tools.lua
+++ modules/neovim/lua/user/lsp/rust-tools.lua
@@ -3,11 +3,27 @@ if not status_ok then
   return
 end
 
-local opts = {}
-
-rt.setup({
+local opts = {
   server = {
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
+    settings = {
+      ["rust-analyzer"] = {
+        procMacro = {
+          enable = false,
+          attributes = {
+            enable = false,
+          }
+        },
+        cachePriming = {
+          enable = false, -- https://github.com/rust-lang/rust-analyzer/issues/14495#issuecomment-1498571185
+        },
+        lru = {
+          capacity = 256, -- https://github.com/rust-lang/rust-analyzer/issues/13916#issuecomment-1396705571
+        },
+      },
+    }
   },
-})
+}
+
+rt.setup(opts)
