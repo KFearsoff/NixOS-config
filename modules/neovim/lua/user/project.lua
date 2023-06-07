9c0a5c9733163777bd12cd6c65452e2fd8717a4c
KFearsoff <kfearsoff@gmail.com>
2023-06-07 11:56:58 +0400

    fix: detect projects correctly

--- modules/neovim/lua/user/project.lua
+++ modules/neovim/lua/user/project.lua
@@ -2,7 +2,10 @@ local status_ok, project = pcall(require, "project_nvim")
 if not status_ok then
 	return
 end
-project.setup()
+
+project.setup({
+  detection_methods = {"pattern", "lsp"},
+})
 
 local tele_status_ok, telescope = pcall(require, "telescope")
 if not tele_status_ok then
