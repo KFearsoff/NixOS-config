6d87a99a499d91e4c686757ce6644d933a430acd
KFearsoff <kfearsoff@gmail.com>
2023-06-07 11:56:30 +0400

    fix: scrolling in toggleterm

--- modules/neovim/lua/user/toggleterm.lua
+++ modules/neovim/lua/user/toggleterm.lua
@@ -16,12 +16,13 @@ toggleterm.setup({
 	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
+  auto_scroll = false,
 })
 
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts) -- causes hangs while scrolling down
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
