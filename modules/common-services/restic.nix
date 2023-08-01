1750483e771a36a75debc2c91bab41a51f68d6a4
KFears <kfearsoff@gmail.com>
2023-08-01 19:50:11 +0400

    refactor(restic): better structure

--- modules/common-services/restic.nix
+++ modules/common-services/restic.nix
@@ -7,11 +7,43 @@
 }:
 with lib; let
   cfg = config.nixchad.restic;
-  backup-builder = template: builtins.mapAttrs (_: value: template // value);
-  usb-template = {
+  backup-builder = builtins.mapAttrs (_: recursiveUpdate template);
+  template = {
     passwordFile = "/secrets/usb-flash-drive-backup";
     user = username;
     repository = "/run/media/${username}/Ventoy/restic-backups";
+  };
+
+  systemd-collector = foldl' recursiveUpdate {} systemd-builder;
+  systemd-builder = builtins.map systemd-fn (builtins.attrNames backupLocations);
+  systemd-fn = name: {
+    services."restic-backups-${name}" = {
+      after = ["restic-backups-init-repo.service"];
+      wantedBy = ["restic-backups-init-repo.service"];
+    };
+    timers."restic-backups-${name}".enable = false;
+  };
+
+  backupLocations = {
+    photos = {
+      paths = [
+        "/home/${username}/Pictures/Photos"
+        "/home/${username}/Pictures/Photos-phone"
+      ];
+      extraBackupArgs = ["--verbose" "--host common" "--tag photos"];
+    };
+    secrets = {
+      paths = [
+        "/secrets"
+      ];
+      extraBackupArgs = ["--verbose" "--tag secrets"];
+    };
+    stuff = {
+      paths = [
+        "/home/${username}/Sync"
+      ];
+      extraBackupArgs = ["--verbose" "--host common" "--tag stuff"];
+    };
   };
 in {
   options.nixchad.restic = {
@@ -19,60 +51,29 @@ in {
     usb-backups = mkEnableOption "backups to USB drive";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable && cfg.usb-backups) {
     hm.home.packages = [pkgs.restic];
 
-    services.restic.backups = optionalAttrs cfg.usb-backups (backup-builder usb-template {
-      init-usb-repo = {
-        initialize = true;
-        backupPrepareCommand = "${pkgs.coreutils}/bin/sleep 1";
-      };
-      photos-usb = {
-        paths = [
-          "/home/${username}/Pictures/Photos"
-          "/home/${username}/Pictures/Photos-phone"
-        ];
-        extraBackupArgs = ["--verbose" "--host common" "--tag photos"];
-      };
-      secrets-usb = {
-        paths = [
-          "/secrets"
-        ];
-        extraBackupArgs = ["--verbose" "--tag secrets"];
-      };
-      stuff-usb = {
-        paths = [
-          "/home/${username}/Sync"
-        ];
-        extraBackupArgs = ["--verbose" "--host common" "--tag stuff"];
-      };
-    });
+    services.restic.backups = backup-builder ({
+        init-repo = {
+          initialize = true;
+          backupPrepareCommand = "${pkgs.coreutils}/bin/sleep 1";
+        };
+      }
+      // backupLocations);
 
     systemd = let
       device = "dev-disk-by\\x2duuid-8277\\x2dDD24.device";
     in
-      optionalAttrs cfg.usb-backups {
-        services.restic-backups-photos-usb = {
-          after = [device "restic-backups-init-usb-repo.service"];
-          wantedBy = [device];
-        };
-        timers.restic-backups-photos-usb.enable = false;
-        services.restic-backups-secrets-usb = {
-          after = [device "restic-backups-init-usb-repo.service"];
-          wantedBy = [device];
-        };
-        timers.restic-backups-secrets-usb.enable = false;
-        services.restic-backups-stuff-usb = {
-          after = [device "restic-backups-init-usb-repo.service"];
-          wantedBy = [device];
-        };
-        timers.restic-backups-stuff-usb.enable = false;
-        services.restic-backups-init-usb-repo = {
-          after = [device];
-          wantedBy = [device];
-          serviceConfig.ExecStart = ["${pkgs.coreutils}/bin/echo \"restic repository initialized\""];
-        };
-        timers.restic-backups-init-usb-repo.enable = false;
-      };
+      optionalAttrs cfg.usb-backups (recursiveUpdate
+        {
+          services.restic-backups-init-repo = {
+            after = [device];
+            wantedBy = [device];
+            serviceConfig.ExecStart = ["${pkgs.coreutils}/bin/echo \"restic repository initialized\""];
+          };
+          timers.restic-backups-init-repo.enable = false;
+        }
+        systemd-collector);
   };
 }
