1dd1ca03fba0c50fc972480abded86e50fa27cf3
KFears <kfearsoff@gmail.com>
2023-08-07 16:30:02 +0400

    fix(cloudberry): use networkd

--- hosts/cloudberry/default.nix
+++ hosts/cloudberry/default.nix
@@ -1,11 +1,11 @@
 {
   inputs,
   username,
+  lib,
   ...
 }: {
   imports = [
     (import ./disko.nix {})
-    ./networking.nix
     ./hardware-configuration.nix
     inputs.disko.nixosModules.disko
   ];
@@ -18,6 +18,33 @@
   nixchad.boot.bootloader = "grub-noefi";
   nixchad.photoprism.enable = false;
   zramSwap.enable = true;
+
+  networking = {
+    nameservers = lib.mkForce [
+      "8.8.8.8"
+      "1.1.1.1"
+    ];
+    useDHCP = false;
+    nat.externalInterface = "enp1s0";
+  };
+  systemd.network = {
+    enable = true;
+
+    networks.enp1s0 = {
+      matchConfig.Name = "enp1s0";
+      gateway = ["172.31.1.1" "fe80::1"];
+      address = ["37.27.0.141/32" "2a01:4f9:c012:a517::1/64" "fe80::9400:2ff:fe2e:b2c9/64"];
+      routes = [
+        {
+          routeConfig.Destination = "172.31.1.1";
+        }
+        {
+          routeConfig.Destination = "fe80::1";
+        }
+      ];
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
 
   nixchad.impermanence.presets = {
     enable = true;
