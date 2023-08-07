9212c901824c4f167d8a06c07a26e38380f24591
KFears <kfearsoff@gmail.com>
2023-08-07 16:36:15 +0400

    fix: migrate nix-settings to RFC42

--- modules/system/nixconf.nix
+++ modules/system/nixconf.nix
@@ -3,24 +3,27 @@
   nix = {
     package = pkgs.nixUnstable;
     settings = {
-      auto-optimise-store = true;
       trusted-users = ["@wheel"];
+      auto-optimise-store = true;
+      keep-going = true;
+      keep-outputs = true;
+      keep-derivations = true;
+      warn-dirty = false;
+
+      experimental-features = ["nix-command" "flakes" "ca-derivations"];
+
+      builders-use-substitutes = true;
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       ];
     };
-    # keep-outputs and keep-derivations options are great for development shells
-    extraOptions = ''
-      experimental-features = nix-command flakes ca-derivations
-      keep-outputs = true
-      keep-derivations = true
-      warn-dirty = false
-      builders-use-substitutes = true
-    '';
     gc = {
       automatic = true;
       options = "--delete-older-than 7d";
