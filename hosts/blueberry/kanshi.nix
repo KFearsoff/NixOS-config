80ef2cfce506ea6280e4f246ac5b747f452519c4
KFears <kfearsoff@gmail.com>
2023-08-26 16:51:53 +0400

    feat: add more kanshi configs to blueberry

--- hosts/blueberry/kanshi.nix
+++ hosts/blueberry/kanshi.nix
@@ -3,13 +3,30 @@
     services.kanshi = {
       enable = true;
       profiles = {
-        home = {
+        unplugged = {
           outputs = [
             {
               criteria = "eDP-2";
               status = "enable";
               position = "0,0";
               mode = "1920x1080@165";
+              adaptiveSync = true;
+            }
+          ];
+        };
+        plugged = {
+          outputs = [
+            {
+              criteria = "eDP-2";
+              status = "enable";
+              position = "0,0";
+              mode = "1920x1080@165";
+              adaptiveSync = true;
+            }
+            {
+              criteria = "HDMI-A-1";
+              status = "enable";
+              position = "0,-1440";
               adaptiveSync = true;
             }
           ];
