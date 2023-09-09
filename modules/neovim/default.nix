256be9d59f4be0535d429c2e6b667f20f687a19d
KFears <kfearsoff@gmail.com>
2023-09-09 14:04:24 +0400

    refactor: move keymaps and options higher

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -151,9 +151,9 @@ in {
           vim.loader.enable() -- byte-compile and cache lua files
 
           require "lib"
-          require "user.options"
+          require "options"
+          require "keymaps"
           require "user.colorscheme"
-          require "user.keymaps"
           require "user.cmp"
           require "user.lsp"
           require "user.telescope"
