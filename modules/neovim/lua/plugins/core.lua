ff9b735323b0302d67fa37d66dee1cb81ed5645e
KFears <kfearsoff@gmail.com>
2023-09-16 04:20:40 +0400

    refactor: structure config like LazyVim

--- modules/neovim/lua/plugins/core.lua
+++ modules/neovim/lua/plugins/core.lua
@@ -1,6 +1,14 @@
 require("config").init()
 
 return {
-  { "folke/lazy.nvim",         dev = true, version = "*" },
-  { name = "config-as-plugin", dir = "..", main = "config", priority = 10000, lazy = false, config = true, cond = true },
+  { "folke/lazy.nvim", dev = true, version = "*" },
+  {
+    name = "config-as-plugin",
+    dir = "..",
+    main = "config",
+    priority = 10000,
+    lazy = false,
+    config = true,
+    cond = true,
+  },
 }
