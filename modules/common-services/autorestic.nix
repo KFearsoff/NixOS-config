47e3a1a5c4e6a3254f7d4bd46d8d883787f184c9
KFears <kfearsoff@gmail.com>
2023-09-17 11:38:28 +0400

    feat: setup restic and autorestic

--- modules/common-services/autorestic.nix
+++ modules/common-services/autorestic.nix
@@ -1,32 +1,33 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
+{pkgs, ...}: let
   autoresticCfg =
     pkgs.writeText "autorestic.yaml" (builtins.readFile ./autorestic.yaml);
 in {
   systemd.tmpfiles.rules = [
     "d /var/lib/autorestic 0755 nixchad users 10d -"
+    "L /var/lib/autorestic/autorestic.yaml - - - - ${autoresticCfg}"
   ];
 
   systemd.services."autorestic-backup" = {
-    path = [pkgs.openssh pkgs.restic];
+    path = [pkgs.openssh pkgs.restic pkgs.coreutils];
     environment = {
       RESTIC_CACHE_DIR = "%C/restic-backups";
     };
-    preStart = ''
-      ${lib.getBin pkgs.envsubst}/bin/envsubst -o "/tmp/autorestic-substituted.yaml" -i "${autoresticCfg}"
-    '';
     serviceConfig = {
-      ExecStart = "${pkgs.autorestic}/bin/autoresticCfg -c /tmp/autorestic-substituted.yaml --ci cron";
-      EnvironmentFile = /secrets/restic-backup-linus;
+      ExecStart = "${pkgs.autorestic}/bin/autorestic -c /var/lib/autorestic/autorestic.yaml --ci cron";
       User = "nixchad";
       Type = "oneshot";
       RuntimeDirectory = "restic-backups";
       CacheDirectory = "restic-backups";
       CacheDirectoryMode = "0700";
       PrivateTmp = true;
+    };
+  };
+
+  systemd.timers."autorestic-backup" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnCalendar = "hourly";
+      Persistent = true;
     };
   };
 }
--- modules/common-services/restic.nix
+++ modules/common-services/restic.nix
@@ -7,44 +7,7 @@
 }:
 with lib; let
   cfg = config.nixchad.restic;
-  backup-builder = builtins.mapAttrs (_: recursiveUpdate template);
-  template = {
-    passwordFile = "/secrets/usb-flash-drive-backup";
-    user = username;
-    repository = "/run/media/${username}/Ventoy/restic-backups";
-  };
-
-  systemd-collector = foldl' recursiveUpdate {} systemd-builder;
-  systemd-builder = builtins.map systemd-fn (builtins.attrNames backupLocations);
-  systemd-fn = name: {
-    services."restic-backups-${name}" = {
-      after = ["restic-backups-init-repo.service"];
-      wantedBy = ["restic-backups-init-repo.service"];
-    };
-    timers."restic-backups-${name}".enable = false;
-  };
-
-  backupLocations = {
-    photos = {
-      paths = [
-        "/home/${username}/Pictures/Photos"
-        "/home/${username}/Pictures/Photos-phone"
-      ];
-      extraBackupArgs = ["--verbose" "--host common" "--tag photos"];
-    };
-    secrets = {
-      paths = [
-        "/secrets"
-      ];
-      extraBackupArgs = ["--verbose" "--tag secrets"];
-    };
-    stuff = {
-      paths = [
-        "/home/${username}/Sync"
-      ];
-      extraBackupArgs = ["--verbose" "--host common" "--tag stuff"];
-    };
-  };
+  device = "dev-disk-by\\x2duuid-8277\\x2dDD24.device";
 in {
   options.nixchad.restic = {
     enable = mkEnableOption "Restic backups";
@@ -54,26 +17,27 @@ in {
   config = mkIf (cfg.enable && cfg.usb-backups) {
     hm.home.packages = [pkgs.restic];
 
-    services.restic.backups = backup-builder ({
-        init-repo = {
-          initialize = true;
-          backupPrepareCommand = "${pkgs.coreutils}/bin/sleep 1";
-        };
-      }
-      // backupLocations);
-
-    systemd = let
-      device = "dev-disk-by\\x2duuid-8277\\x2dDD24.device";
-    in
-      optionalAttrs cfg.usb-backups (recursiveUpdate
-        {
-          services.restic-backups-init-repo = {
-            after = [device];
-            wantedBy = [device];
-            serviceConfig.ExecStart = ["${pkgs.coreutils}/bin/echo \"restic repository initialized\""];
-          };
-          timers.restic-backups-init-repo.enable = false;
-        }
-        systemd-collector);
+    systemd.services."usb-restic-backup" = {
+      path = [pkgs.openssh];
+      environment = {
+        RESTIC_CACHE_DIR = "%C/restic-backups";
+        RESTIC_FROM_PASSWORD_FILE = /secrets/restic-backup-linus;
+        RESTIC_PASSWORD_FILE = /secrets/usb-flash-drive-backup;
+      };
+      preStart = ''
+        ${pkgs.coreutils}/bin/sleep 5 && ${pkgs.restic}/bin/restic snapshots || ${pkgs.restic}/bin/restic init
+      '';
+      after = [device];
+      wantedBy = [device];
+      serviceConfig = {
+        ExecStart = "${pkgs.restic}/bin/restic copy -r /run/media/${username}/Ventoy/restic-backups --from-repo 4.sosiego.sphalerite.org:/backup";
+        User = "nixchad";
+        Type = "oneshot";
+        RuntimeDirectory = "restic-backups";
+        CacheDirectory = "restic-backups";
+        CacheDirectoryMode = "0700";
+        PrivateTmp = true;
+      };
+    };
   };
 }
