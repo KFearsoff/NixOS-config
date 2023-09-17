33d56bf34d7a468643c548f0b9ab96dc12bf0b0b
KFears <kfearsoff@gmail.com>
2023-09-17 12:10:27 +0400

    feat: enable backing up of vaultwarden and pg

--- modules/common-services/autorestic.nix
+++ modules/common-services/autorestic.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
   autoresticCfg =
     pkgs.writeText "autorestic.yaml" (builtins.readFile ./autorestic.yaml);
 in {
@@ -7,21 +12,53 @@ in {
     "L /var/lib/autorestic/autorestic.yaml - - - - ${autoresticCfg}"
   ];
 
-  systemd.services."autorestic-backup" = {
-    path = [pkgs.openssh pkgs.restic pkgs.coreutils];
-    environment = {
-      RESTIC_CACHE_DIR = "%C/restic-backups";
-    };
-    serviceConfig = {
-      ExecStart = "${pkgs.autorestic}/bin/autorestic -c /var/lib/autorestic/autorestic.yaml --ci cron";
-      User = "nixchad";
-      Type = "oneshot";
-      RuntimeDirectory = "restic-backups";
-      CacheDirectory = "restic-backups";
-      CacheDirectoryMode = "0700";
-      PrivateTmp = true;
-    };
-  };
+  systemd.services."autorestic-backup" = lib.mkMerge [
+    {
+      path = [pkgs.openssh pkgs.restic pkgs.coreutils];
+      environment = {
+        RESTIC_CACHE_DIR = "%C/restic-backups";
+      };
+      serviceConfig = {
+        ExecStart = "${pkgs.autorestic}/bin/autorestic -c /var/lib/autorestic/autorestic.yaml --ci cron";
+        User = "nixchad";
+        Type = "oneshot";
+        RuntimeDirectory = "restic-backups";
+        CacheDirectory = "restic-backups";
+        CacheDirectoryMode = "0700";
+        PrivateTmp = true;
+      };
+    }
+    (lib.mkIf config.services.postgresql.enable {
+      serviceConfig = {
+        ExecStartPre = ''
+          echo 'dumping PostgreSQL database'
+          ${pkgs.shadow.su}/bin/su postgres -c ${config.services.postgresql.package}/bin/pg_dumpall > /secrets/pgdump.sql
+          (( $? )) && exit $?
+        '';
+
+        ExecStartPost = ''
+          rm -v /secrets/pgdump.sql
+        '';
+      };
+    })
+    (lib.mkIf config.services.vaultwarden.enable {
+      serviceConfig = lib.mkForce {
+        ExecStartPre = ''
+          echo 'dumping PostgreSQL database'
+          ${pkgs.shadow.su}/bin/su postgres -c ${config.services.postgresql.package}/bin/pg_dumpall > /secrets/pgdump.sql
+          (( $? )) && exit $?
+          echo 'copying Vaultwarden data'
+          cp --reflink=auto -r /var/lib/bitwarden_rs /secrets/vaultwarden
+          (( $? )) && exit $?
+        '';
+
+        execStartPost = ''
+          rm -v /secrets/pgdump.sql
+          rm -rfv /secrets/vaultwarden
+        '';
+      };
+    })
+  ];
 
   systemd.timers."autorestic-backup" = {
     wantedBy = ["timers.target"];
