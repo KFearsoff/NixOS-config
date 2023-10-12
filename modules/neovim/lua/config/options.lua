8c2f8a070989be5790cf9cb9e8955334994550ce
KFears <kfearsoff@gmail.com>
2023-10-12 19:42:51 +0400

    feat: tweak some options

--- modules/neovim/lua/config/options.lua
+++ modules/neovim/lua/config/options.lua
@@ -5,9 +5,10 @@ vim.g.maplocalleader = "\\"
 local opt = vim.opt
 
 opt.autowrite = true -- Enable auto write
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.clipboard = "unnamedplus" -- Sync with systemclipboard
+opt.completeopt = "menuone,noselect,noinsert"
+opt.conceallevel = 0 -- So that `` is visible in markdown
+opt.showtabline = 2 -- Always show tabs
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
 opt.expandtab = true -- Use spaces instead of tabs
@@ -44,7 +45,7 @@ opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+opt.wrap = true -- Enable line wrap
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
