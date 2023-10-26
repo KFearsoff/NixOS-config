325020be2dffdf3cc74591c5ed535df4fce5ad9a
KFears <kfearsoff@gmail.com>
2023-10-26 20:42:18 +0400

    bump: 24-10-2023

--- modules/neovim/lua/plugins/lsp/keymaps.lua
+++ modules/neovim/lua/plugins/lsp/keymaps.lua
@@ -146,7 +146,7 @@ function M.on_attach(client, buffer)
       opts.has = nil
       opts.silent = opts.silent ~= false
       opts.buffer = buffer
-      vim.keymap.set(keys.mode or "n", keys[1], keys[2], opts)
+      vim.keymap.set(keys.mode or "n", keys.lhs, keys.rhs, opts)
     end
   end
 end
