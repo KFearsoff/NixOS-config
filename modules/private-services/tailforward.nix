713447c798836d60910acfc10dfb76b2ec8af5c0
KFears <kfearsoff@gmail.com>
2023-11-04 16:43:01 +0400

    fix: tailforward

--- modules/private-services/tailforward.nix
+++ modules/private-services/tailforward.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  pkgs,
   config,
   lib,
   ...
@@ -7,6 +8,18 @@
 with lib; let
   cfg = config.nixchad.tailforward;
   port = "54321";
+  configFile = pkgs.writeText "tailforward.toml" ''
+    debug = false
+    address = "127.0.0.1:${port}"
+
+    [tailscale]
+    secret_file = "/secrets/tailscale-webhook"
+
+    [telegram]
+    secret_file = "/secrets/telegram"
+    file_format = "Alertmanager"
+    chat_id = -1001864190705
+  '';
 in {
   options.nixchad.tailforward = {
     enable = mkEnableOption "Tailforward, a self-written Rust program to send Tailscale webhook events to Telegram";
@@ -17,10 +30,14 @@ in {
       after = ["network.target"];
       wants = ["network.target"];
       wantedBy = ["default.target"];
-      serviceConfig.ExecStart = "${inputs.tailforward.packages.x86_64-linux.default}/bin/tailforward";
-      environment = {
-        TAILFORWARD_ADDRESS = "127.0.0.1:${port}";
+      serviceConfig = {
+        ExecStart = "${inputs.tailforward.packages.x86_64-linux.default}/bin/tailforward";
+        ConfigurationDirectory = "";
       };
+    };
+
+    environment.etc = {
+      "tailforward/tailforward.toml".source = configFile;
     };
 
     services.nginx = {
