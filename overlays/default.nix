ac3e968f5aafc795be0a5f3f29cf13365c1fb5ee
KFears <kfearsoff@gmail.com>
2023-12-10 21:34:26 +0400

    bump: 10-12-2023

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,23 +1,3 @@
 {
   additions = final: _: import ../pkgs {pkgs = final;};
-  luasnip = _: prev: {
-    vimPlugins = prev.vimPlugins.extend (_: prev': {
-      luasnip = prev'.luasnip.overrideAttrs (_: {
-        pname = "LuaSnip";
-      });
-    });
-  };
-  LazyVim = _: prev: {
-    vimPlugins = prev.vimPlugins.extend (_: prev': {
-      LazyVim = prev'.LazyVim.overrideAttrs (_: {
-        version = "10.8.2";
-        src = prev.pkgs.fetchFromGitHub {
-          owner = "LazyVim";
-          repo = "LazyVim";
-          rev = "879e29504d43e9f178d967ecc34d482f902e5a91";
-          sha256 = "sha256-f+WPP74x9xMVSONz9m4nC4ygcO6axY1ztZHlRh3bbmM=";
-        };
-      });
-    });
-  };
 }
