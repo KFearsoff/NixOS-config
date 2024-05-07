154ca2a03fd57e4732420d6b5a8faa9729e45148
KFears <kfearsoff@gmail.com>
2024-05-07 16:50:16 +0400

    fix: merge full into default.nix

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,10 +1,15 @@
-{
+{lib, ...}: let
+  inherit (lib) mkEnableOption;
+in {
   imports = [
     ./nushell
     ./terminal.nix
     ./starship.nix
     ./sway
     ./zoxide.nix
-    ./full.nix
   ];
+
+  options.nixchad.full = {
+    enable = mkEnableOption "fully customized configuration" // {default = true;};
+  };
 }
--- homeManagerModules/full.nix
+++
@@ -1,7 +0,0 @@
-{lib, ...}: let
-  inherit (lib) mkEnableOption;
-in {
-  options.nixchad.full = {
-    enable = mkEnableOption "fully customized configuration" // {default = true;};
-  };
-}
