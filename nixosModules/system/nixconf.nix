bd19addc4fdf64f6de694a24a1e472fde1a48a01
KFears <kfearsoff@gmail.com>
2024-06-10 19:31:10 +0400

    fix: registry

--- nixosModules/system/nixconf.nix
+++ nixosModules/system/nixconf.nix
@@ -1,5 +1,11 @@
-{
+{inputs, ...}: {
   nix = {
+    # pin all the nixpkgs to the version in the flake
+    registry = {
+      self.flake = inputs.self;
+      nixpkgs.flake = inputs.nixpkgs;
+    };
+
     settings = {
       trusted-users = ["@wheel"];
       auto-optimise-store = true;
