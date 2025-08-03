7685738c62fbdd8f20db9fad1a4489f8486eba38
KFears <kfearsoff@gmail.com>
2025-08-03 13:01:49 +0400

    feat: re-add tab discard

--- nixosModules/gui/firefox.nix
+++ nixosModules/gui/firefox.nix
@@ -26,23 +26,21 @@ in
         enable = true;
         profiles = {
           "default" = {
-            extraConfig =
-              builtins.readFile "${inputs.arkenfox}/user.js"
-              + ''
+            extraConfig = builtins.readFile "${inputs.arkenfox}/user.js" + ''
 
-                // OVERRIDES
+              // OVERRIDES
 
-                user_pref("browser.startup.page", 3); // 0102, session restore
-                user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811, don't clear history; FF 128-135
-                user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812, don't clear history; FF 136+
-                user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504, I hate the margins
-                user_pref("webgl.disabled", false); // required for Zoom (also needs canvas exception)
+              user_pref("browser.startup.page", 3); // 0102, session restore
+              user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811, don't clear history; FF 128-135
+              user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812, don't clear history; FF 136+
+              user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504, I hate the margins
+              user_pref("webgl.disabled", false); // required for Zoom (also needs canvas exception)
 
-                lockPref("signon.rememberSignons", false); // bitwarden is used instead
-                lockPref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // enable CSS styling
-                lockPref("browser.tabs.inTitlebar", "0"); // use native decoration
-                lockPref("media.ffmpeg.vaapi.enabled", true); // enable hardware video acceleration
-              '';
+              lockPref("signon.rememberSignons", false); // bitwarden is used instead
+              lockPref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // enable CSS styling
+              lockPref("browser.tabs.inTitlebar", "0"); // use native decoration
+              lockPref("media.ffmpeg.vaapi.enabled", true); // enable hardware video acceleration
+            '';
 
             userChrome = ''
               /* Hide horizontal tabs on top of the window */
@@ -64,6 +62,7 @@ in
               nur.repos.rycee.firefox-addons.istilldontcareaboutcookies
               nur.repos.rycee.firefox-addons.tree-style-tab
               nur.repos.rycee.firefox-addons.temporary-containers
+              nur.repos.rycee.firefox-addons.auto-tab-discard
             ];
 
             search = {
