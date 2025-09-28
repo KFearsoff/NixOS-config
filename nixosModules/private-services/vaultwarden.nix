bcaa856e6399d918f8d62051fb7c43977362c5e4
KFears <kfearsoff@gmail.com>
2025-09-28 07:26:51 +0400

    feat: persist vaultwarden

--- nixosModules/private-services/vaultwarden.nix
+++ nixosModules/private-services/vaultwarden.nix
@@ -49,6 +49,23 @@ in
       };
     };
 
+    nixchad = {
+      impermanence.persisted.values = [
+        {
+          directories =
+            lib.mkIf
+              (config.nixchad.impermanence.presets.essential && config.nixchad.impermanence.presets.services)
+              [
+                {
+                  directory = "/var/lib/bitwarden_rs";
+                  user = "vaultwarden";
+                  group = "vaultwarden";
+                }
+              ];
+        }
+      ];
+    };
+
     # make sure we don't crash because postgres isn't ready
     systemd.services.vaultwarden.after = [ "postgresql.service" ];
   };
