5a32d43952eb5ddb97437c25191bf810390f480d
KFears <kfearsoff@gmail.com>
2026-01-12 01:15:32 +0400

    feat: update jujutsu config

--- nixosModules/cli/jj.nix
+++ nixosModules/cli/jj.nix
@@ -21,12 +21,40 @@ in
             name = "KFears";
             email = "kfearsoff@gmail.com";
           };
+          templates.commit_trailers = ''
+            format_signed_off_by_trailer(self)
+          '';
+          git = {
+            colocate = true;
+            private-commits = "description('wip:*') | description('private:*')";
+          };
           signing = {
             behavior = "own";
             backend = "ssh";
             key = "~/.ssh/id_ed25519.pub";
           };
           ui.pager = "less -FRX";
+          "--scope" = [
+            {
+              "--when".repositories = [ "~/Documents/Work" ];
+              user = {
+                name = "Egor Terentev";
+                email = "egor@naviteq.io";
+              };
+            }
+            {
+              "--when".repositories = [ "~/Documents/Projects-codeberg" ];
+              user.email = "kfears@riseup.net";
+            }
+            {
+              "--when".repositories = [ "~/Documents/Projects-gerrit" ];
+              user.email = "kfears@riseup.net";
+              templates.commit_trailers = ''
+                format_signed_off_by_trailer(self)
+                ++ if(!trailers.contains_key("Change-Id"), format_gerrit_change_id_trailer(self))
+              '';
+            }
+          ];
         };
       };
     };
