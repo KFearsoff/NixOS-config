2a95a7fd23f42300381747ab8f60d70dac4c1bee
KFears <kfearsoff@gmail.com>
2026-06-29 17:20:52 +0400

    firefox: fix nixos wiki favicon
    Signed-off-by: KFears <kfearsoff@gmail.com>

--- nixosModules/gui/firefox.nix
+++ nixosModules/gui/firefox.nix
@@ -121,7 +121,7 @@ in
 
                 "NixOS Wiki" = {
                   urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
-                  icon = "https://wiki.nixos.org/favicon.png";
+                  icon = "https://wiki.nixos.org/nixos.png";
                   updateInterval = 24 * 60 * 60 * 1000; # every day
                   definedAliases = [ "@nw" ];
                 };
